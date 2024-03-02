40cd976084b826a930e5ba6a54383d88b0e5f56a
Zachary <zach.lee96@gmai.com>
2024-03-02 05:41:50 -0700

    Initial Commit

---
+++ homework 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  homework 1
+//
+//  Created by Zachary Lee on 3/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ homework 1/homework_1App.swift
@@ -0,0 +1,17 @@
+//
+//  homework_1App.swift
+//  homework 1
+//
+//  Created by Zachary Lee on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct homework_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
